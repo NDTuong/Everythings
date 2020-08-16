b22b928d3f9198af9f0a87f355b8955433afdbe0
Nguyen Duy Tuong <51308391+NDTuong@users.noreply.github.com>
2020-08-16 11:11:51 +0700

    Create No_name.py

---
+++ No_name.py
@@ -0,0 +1,8 @@
+//Get all index of a value in a list
+l = [1,2,3,1,1]
+[index for index, value in enumerate(l) if value == 1] //find all index of 1 in list l
+
+//Get all index of a value in a list use numpy
+values = np.array([1,2,3,1,2,4,5,6,3,2,1])
+searchval = 3
+ii = np.where(values == searchval)[0] // [0] because ii = (arr[],...)
